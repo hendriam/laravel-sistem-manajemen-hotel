57f103e01ff97ca76b0aeb23145c9bc75b87c1b2
hendriam <hendriam18@gmail.com>
2025-05-21 23:46:19 +0700

    setup project

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,36 @@
+@extends('app.layout')
+
+@section('css')
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+@endsection
+
+<!-- Content Header (Page header) -->
+@section('content')
+    <div class="content-wrapper">
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">{{ $title }}</h1>
+                    </div><!-- /.col -->
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="{{ url('dashboard') }}">{{ $title }}</a></li>
+                        <li class="breadcrumb-item active">Starter Page</li>
+                        </ol>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
+
+        <!-- Main content -->
+        <div class="content">
+            <div class="container-fluid">
+             
+            </div><!-- /.container-fluid -->
+        </div>
+    </div>
+@endsection
+<!-- /.content -->
