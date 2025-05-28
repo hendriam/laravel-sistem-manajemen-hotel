3e98862f69970699aa3f58fdc8329d5721c39f77
hendriam <hendriam18@gmail.com>
2025-05-28 14:53:40 +0700

    refactor: enhance dashboard view with room and guest statistics

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,13 +4,36 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+use App\Models\Room;
+use App\Models\Guest;
+use App\Models\Reservation;
+use App\Models\Payment;
+use Carbon\Carbon;
 
 class DashboardController extends Controller
 {
     public function index(Request $request) : View
     {
         $title = "Dashboard";
-        return view('dashboard', [
+
+        $totalRooms = Room::count();
+        $availableRooms = Room::where('status', 'available')->count();
+        $occupiedRooms = Room::whereIn('status', ['occupied', 'booked'])->count();
+        
+        $totalGuests = Guest::count();
+
+        $now = Carbon::now();
+        $monthlyReservations = Reservation::whereMonth('check_in_date', $now->month)->count();
+        $monthlyIncome = Payment::whereMonth('payment_date', $now->month)->sum('amount');
+        
+        return view('dashboard',compact(
+            'totalRooms',
+            'availableRooms',
+            'occupiedRooms',
+            'totalGuests',
+            'monthlyReservations',
+            'monthlyIncome'
+        ),[
             'title' => $title,
         ]);
     }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -42,7 +42,93 @@
         <!-- Main content -->
         <div class="content">
             <div class="container-fluid">
-                
+                <div class="row">
+                    <div class="col-lg-3 col-6">
+                        <!-- small box -->
+                        <div class="small-box bg-info">
+                            <div class="inner">
+                                <h3>{{ $totalRooms }}</h3>
+                                <p>Total Semua Kamar</p>
+                            </div>
+                            <div class="icon">
+                                <i class="ion ion-bag"></i>
+                            </div>
+                            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-6">
+                        <!-- small box -->
+                        <div class="small-box bg-success">
+                            <div class="inner">
+                                <h3>{{ $availableRooms }}</h3>
+                                <p>Total Kamar Tersedia</p>
+                            </div>
+                            <div class="icon">
+                                <i class="ion ion-stats-bars"></i>
+                            </div>
+                            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-6">
+                        <!-- small box -->
+                        <div class="small-box bg-warning">
+                            <div class="inner">
+                                <h3>{{ $occupiedRooms }}</h3>
+                                <p>Total Kamar Terisi</p>
+                            </div>
+                            <div class="icon">
+                                <i class="ion ion-pie-graph"></i>
+                            </div>
+                            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-6">
+                        <!-- small box -->
+                        <div class="small-box bg-success">
+                            <div class="inner">
+                                <h3>{{ $totalGuests }}</h3>
+                                <p>Total Semua Tamu</p>
+                            </div>
+                            <div class="icon">
+                                <i class="ion ion-stats-bars"></i>
+                            </div>
+                            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-3 col-6">
+                        <!-- small box -->
+                        <div class="small-box bg-info">
+                            <div class="inner">
+                                <h3>{{ $monthlyReservations }}</h3>
+                                <p>Total Reservasi  Bulan Ini</p>
+                            </div>
+                            <div class="icon">
+                                <i class="ion ion-bag"></i>
+                            </div>
+                            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-6">
+                        <!-- small box -->
+                        <div class="small-box bg-success">
+                            <div class="inner">
+                                <h3>Rp {{ number_format($monthlyIncome, 0, ',', '.') }}</h3>
+                                <p>Pendapatan Bulan Ini</p>
+                            </div>
+                            <div class="icon">
+                                <i class="ion ion-stats-bars"></i>
+                            </div>
+                            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+                </div>
             </div><!-- /.container-fluid -->
         </div>
         
