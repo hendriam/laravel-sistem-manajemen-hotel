f5323aeb6c9360fe0b6e57476be48bee0c862ea2
hendriam <hendriam18@gmail.com>
2025-05-28 12:46:54 +0700

    refactor: simplify invoice generation and enhance invoice layout

--- app/Http/Controllers/PrintController.php
+++ app/Http/Controllers/PrintController.php
@@ -17,48 +17,8 @@ class PrintController extends Controller
         $reservation->duration = $checkIn->diffInDays($checkOut);
         $reservation->total_paid = $reservation->payments->sum('amount');
 
-        $print = $this->printInvoice($reservation);
-
         return view('print.invoice',[
-            'print' => $print,
+            'reservation' => $reservation
         ]);
-    }
-
-    public function printInvoice($reservation)
-    {
-        $print = '';
-        $print.='<div style="width:100%;padding-bottom:20px;">
-                  <table>
-                    <tr>
-                        <td align="center" colspan="4"><strong>Bukti Pembayaran</strong></td>
-                    </tr>
-                    <tr>
-                        <td>Nama Tamu</td><td>: '.$reservation->guest->name.'</td><td>Jumlah Hari</td><td>: '.$reservation->duration.'</td>
-                    </tr>
-                    <tr>
-                        <td>Kamar</td><td>: '.$reservation->room->room_number.'</td><td>Total Pembayaran</td><td>: '.$reservation->total_paid.'</td>
-                    </tr>
-                    <tr>
-                        <td>Tgl. Check-in</td><td>: '.$reservation->check_in_date.'</td><td></td><td>: </td>
-                    </tr>
-                    <tr>
-                        <td>Tgl. Check-out</td><td>: '.$reservation->check_out_date.'</td><td></td><td>: </td>
-                    </tr>
-                    <tr></tr>
-                    <tr></tr>
-                    <tr>
-                        <td colspan="4" align="center">
-                        	<strong>Terima Kasih</strong>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td colspan="4" align="center">
-                        	<strong>Hotel Sandika, Jln. Bandung No. 40</strong>
-                        </td>
-                    </tr>
-                </table>
-                </div>';
-
-        return $print;
     }
 }
--- resources/views/print/invoice.blade.php
+++ resources/views/print/invoice.blade.php
@@ -9,19 +9,39 @@
         @media print {
             body {
                 font-family : "Open Sans", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
-                width: 100%;
+                width: 90%;
             }
         }
         @page  
         { 
             size: auto;
-            margin: 2mm 2mm 2mm 2mm;
+            margin: 3mm 3mm 3mm 3mm;
         } 
         body {
             font-family : "Open Sans", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
             font-family: monospace;
             font-size: 14px;
+                width: 90%;
         }
+        .header{
+            display: flex;
+            justify-content: space-between;
+        }
+
+        .header > div {
+            line-height: 0.1;
+        }
+
+        .table-reservation, .table-reservation th, .table-reservation td {
+            border: 1px solid black;
+            padding: 5px;
+        }
+
+        .table-reservation{
+            width: 100%;
+            border-collapse: collapse;
+        }
+
         #divPrint{
             letter-spacing: -1px;
             line-height: 70%;
@@ -37,7 +57,69 @@
 </head>
 <body>
     <div id="divPrint">
-        <?php echo $print; ?>
+        <div class="header">
+            <div>
+                <h3>Hotel Sandika</h3>
+                <p>Jln. Bandung No. 40</p>
+                <p>Telp: (022) 1234567</p>
+                <p>Email: hotelsandika@gmail.com</p>
+            </div>
+            <div>
+                <h2>INVOICE</h2>
+                <p>No. Invoice: {{ $reservation->reservation_number }}</p>
+                <p>Tanggal: {{ \Carbon\Carbon::parse($reservation->created_at)->format('d M Y') }}</p>
+            </div>
+        </div>
+        <div class="content">
+            <h3 style="margin-bottom:5px;">Customer</h3>
+            <table>
+                <tr>
+                    <td>Nama</td>
+                    <td>:</td>
+                    <td>{{ $reservation->guest->name }}</td>
+                </tr>
+                <tr>
+                    <td>Alamat</td>
+                    <td>:</td>
+                    <td>{{ $reservation->guest->address }}</td>
+                </tr>
+                <tr>
+                    <td>No. Telepon</td>
+                    <td>:</td>
+                    <td>{{ $reservation->guest->phone }}</td>
+                </tr>
+            </table>
+
+            <h3 style="margin-bottom:5px;">Reservasi</h3>
+            <table class="table-reservation">
+                <tr>
+                    <th style="text-align: start">No.</th>
+                    <th style="text-align: start">Kamar</th>
+                    <th style="text-align: end">Qty</th>
+                    <th style="text-align: end">Harga Kamar</th>
+                    <th style="text-align: end">Total</th>
+                </tr>
+                <tr>
+                    <td style="text-align: start">1.</td>
+                    <td style="text-align: start">Kamar {{ $reservation->room->room_number }} ( Tipe {{ $reservation->room->roomType->name }}, {{ $reservation->room->floor->name }})</td>
+                    <td style="text-align: end">{{ $reservation->duration }}</td>
+                    <td style="text-align: end">{{ number_format($reservation->room->price, 0, ',', '.') }}</td>
+                    <td style="text-align: end">{{ number_format($reservation->room->price * $reservation->duration, 0, ',', '.') }}</td>
+                </tr>
+                <tr>
+                    <td colspan="3" style="text-align: end; border:0px;"><strong>Total</strong></td>
+                    <td colspan="2" style="text-align: end"><strong>{{ number_format($reservation->room->price * $reservation->duration, 0, ',', '.') }}</strong></td>
+                </tr>
+                <tr>
+                    <td colspan="3" style="text-align: end; border:0px;"><strong>Pajak</strong></td>
+                    <td colspan="2" style="text-align: end"><strong>0</strong></td>
+                </tr>
+                <tr>
+                    <td colspan="3" style="text-align: end; border:0px;"><strong>Total Pembayaran</strong></td>
+                    <td colspan="2" style="text-align: end"><strong>{{ number_format($reservation->room->price * $reservation->duration, 0, ',', '.') }}</strong></td>
+                </tr>
+            </table>
+        </div>
     </div>
 </body>
 </html>
